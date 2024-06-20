c6d2472988468ab863e95ffd94bf8f7f93b54456
theDasturchi <e-mail pls>
2024-06-20 19:54:35 +0500

    generate html for order summer

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,10 @@
-export const cart = [];
+export const cart = [
+  { productId: "77919bbe-0e56-475b-adde-4f24dfed3a04", quantity: 1 },
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 2,
+  },
+];
 export function addToCard(productId) {
   let matchingItem;
   cart.forEach((cartItem) => {
