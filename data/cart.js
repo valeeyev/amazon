3b53f99a7d15a23a6b3e2961891250d3f058cfc9
theDasturchi <e-mail pls>
2024-06-22 12:53:27 +0500

    remove cart function created

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [
+export let cart = [
   { productId: "77919bbe-0e56-475b-adde-4f24dfed3a04", quantity: 1 },
   {
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
@@ -21,4 +21,14 @@ export function addToCard(productId) {
       quantity: 1,
     });
   }
+}
+
+export function deleteCartItem(productId) {
+  const newArray = [];
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newArray.push(cartItem);
+    }
+  });
+  cart = newArray;
 }
